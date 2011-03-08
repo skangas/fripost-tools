d99fe7fb2c3fe992b55fc4cf7048bfdee9f11221
Stefan Kangas <skangas@skangas.se>
2011-03-08 13:36:19 +0100

    Add logger stub

---
+++ lib/Fripost/Logger.pm
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use 5.010_000;
+use warnings;
+use strict;
+
+=head1 NAME
+
+Logger.pm - 
+
+=cut
+
+our $VERSION = '0.01';
+
+use Moose;
+use namespace::autoclean;
+
+sub log_adduser {
+    
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+=head1 AUTHOR
+
+Stefan Kangas C<< <skangas at skangas.se> >>
+
+=head1 COPYRIGHT
+
+Copyright 2010 Stefan Kangas.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as perl itself.
+
+=cut
+
+1; # End of Logger.pm
+
